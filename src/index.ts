4f7596a3587c9de6fa7c83a76cc5374cc666ca6a
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2025-04-21 09:41:06 -0300

    feat: variaveis de ambientes e configuração de builds

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from "./server/Server";
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log("Servidor instanciado e operando na porta ", PORT));
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { StatusCodes } from 'http-status-codes';
 
 const router = Router();
 
-router.get('/', (_, res) => {
+router.get('/', (_, res: Response) => {
   res.status(200).send('Olá dev!');
 });
 
-router.post('/api/teste', (req, res) => {
+router.post('/api/teste', (req: Request, res: Response) => {
   res.status(StatusCodes.CREATED).json(req.body);
 });
 
