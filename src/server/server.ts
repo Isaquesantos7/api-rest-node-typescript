ba51476ed03d3e18aed91c39b0f8b7af0ae34cb6
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2025-04-20 21:26:46 -0300

    feat: add eslint and editor config

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const server = express();
 
-server.get('/api/teste', (req, res) => {
-    res.status(200).json({"message": "sucesso."});
+server.get('/api/teste', (_, res) => {
+  res.status(200).json({"message": "sucesso."});
 });
 
 export { server };
