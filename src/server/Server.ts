fe90d0a4810b3ee81fcb6c62c7ab193fa4ce841d
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2025-04-22 20:38:26 -0300

    feat: traduzindo as validações do yup

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import 'dotenv/config';
 
-import { router } from "../routes";
+import "./shared/services/TranslationsYup";
+import { router } from "./routes";
 
 const server = express();
 
